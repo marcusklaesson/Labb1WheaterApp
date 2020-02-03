26b2bd1f6e579451eabd63261f644a70af09cfb2
marcusklaesson <55803843+marcusklaesson@users.noreply.github.com>
2020-02-03 11:19:03 +0100

    Initial Commit

---
+++ Labb1WheaterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Labb1WheaterApp
+//
+//  Created by Marcus Klaesson on 2020-02-03.
+//  Copyright © 2020 Marcus Klaesson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
